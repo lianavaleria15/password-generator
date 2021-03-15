89183e8f35314344c94630b5e5902c5ccde1f7a5
liana valeria <liana.valeria15@gmail.com>
2021-03-15 14:05:36 +0000

    declare character type  arrays in global memory

--- script.js
+++ script.js
@@ -1,16 +1,93 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Declare characters arrays in global memory
+lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+specialCharArray = [
+  "@",
+  "%",
+  "+",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ")",
+  "(",
+  "{",
+  "}",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+];
 // Declare function which generates password
 function generatePassword() {
-	// Create a variable to store our generated password
-	var password = ''
+  // Create a variable to store our generated password
+  var password = "";
 
-	// Logic would go here
-	...
+  // Logic would go here
 
-	// Return our created password
-	return password;
+  // Return our created password
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +95,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
