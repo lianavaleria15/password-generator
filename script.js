07938befd83938697f2597a2744f47714225455b
liana valeria <liana.valeria15@gmail.com>
2021-03-17 21:38:18 +0000

    add random array and random character function; add random characters to password array; convert password array to string and return password

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 //Declare characters arrays in global memory
-lowerCaseArray = [
+const lowerCaseArray = [
   "a",
   "b",
   "c",
@@ -30,7 +30,8 @@ lowerCaseArray = [
   "y",
   "z",
 ];
-upperCaseArray = [
+
+const upperCaseArray = [
   "A",
   "B",
   "C",
@@ -58,8 +59,10 @@ upperCaseArray = [
   "Y",
   "Z",
 ];
-numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-specialCharArray = [
+
+const numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const specialCharArray = [
   "@",
   "%",
   "+",
@@ -79,69 +82,86 @@ specialCharArray = [
   "-",
   "_",
 ];
+
 let isLowerCase;
 let isUpperCase;
 let isNumericChar;
 let isSpecialChar;
+
 // Declare new array to contain types of character chosen
 const chosenCharArray = [];
 // Create prompt password length and convert length of password as a string in a number
-const PasswordNumber = () => {
+
+// //get random index of options array
+//
+// // get random character from the random array
+
+// Declare function which generates password
+function generatePassword() {
+  const passwordArray = [];
   const passwordLength = prompt(
     "How many characters would you want your password to be?"
   );
   console.log(passwordLength);
   const passwordLengthNumber = parseInt(passwordLength, 10);
   console.log(passwordLengthNumber);
-};
 
-//validation password length and confirm type of characters
-if (passwordLengthNumber >= 8 && passwordLengthNumber <= 128) {
-  // confirm popup box for lowercase characters
-  const isLowerCase = confirm(
-    "Do you want password to include lowercase characters?"
-  );
-  console.log(isLowerCase);
-  //confirm popup box for uppercase characters
-  const isUpperCase = confirm(
-    "Do you want password to include uppercase characters?"
-  );
-  console.log(isUpperCase);
-  //confirm popup box for numeric characters
-  const isNumericChar = confirm(
-    "Do you want password to include numeric characters?"
-  );
-  console.log(isNumericChar);
-  //confirm popup box for special character
-  const isSpecialChar = confirm(
-    "Do you want password to include special characters?"
-  );
-  console.log(isSpecialChar);
-} else {
-  alert("Password should be between 8 and 128 characters");
-}
+  //validation password length and confirm type of characters
+  if (passwordLengthNumber >= 8 && passwordLengthNumber <= 128) {
+    // confirm popup box for lowercase characters
+    isLowerCase = confirm(
+      "Do you want password to include lowercase characters?"
+    );
+    console.log(isLowerCase);
+    //confirm popup box for uppercase characters
+    isUpperCase = confirm(
+      "Do you want password to include uppercase characters?"
+    );
+    console.log(isUpperCase);
+    //confirm popup box for numeric characters
+    isNumericChar = confirm(
+      "Do you want password to include numeric characters?"
+    );
+    console.log(isNumericChar);
+    //confirm popup box for special character
+    isSpecialChar = confirm(
+      "Do you want password to include special characters?"
+    );
+    console.log(isSpecialChar);
+  } else {
+    alert("Password should be between 8 and 128 characters");
+  }
 
-// add validation function for character type and forms a new array with type characters chose
-if (isLowerCase) {
-  chosenCharArray.push(lowerCaseArray);
-} else if (isUpperCase) {
-  chosenCharArray.push(upperCaseArray);
-} else if (isNumericChar) {
-  chosenCharArray.push(numericArray);
-} else if (isSpecialChar) {
-  chosenCharArray.push(specialCharArray);
-} else {
-  alert("At least one type of character should be");
-}
-//get random index of options array
-let randomArray =
-  chosenCharArray[Math.floor(Math.random() * chosenCharArray.length)];
-// get random character from the random array
+  // add validation function for character type and forms a new array with type characters chose
+  if (isLowerCase) {
+    chosenCharArray.push(lowerCaseArray);
+  }
+  if (isUpperCase) {
+    chosenCharArray.push(upperCaseArray);
+  }
+  if (isNumericChar) {
+    chosenCharArray.push(numericArray);
+  }
+  if (isSpecialChar) {
+    chosenCharArray.push(specialCharArray);
+  }
+  if (!isLowerCase && !isUpperCase && !isNumericChar && !isSpecialChar) {
+    alert("At least one type of character should be selected");
+  }
+  console.log(chosenCharArray);
 
-// Declare function which generates password
-function generatePassword() {
+  for (let i = 0; i < passwordLengthNumber; i++) {
+    let randomArray =
+      chosenCharArray[Math.floor(Math.random() * chosenCharArray.length)];
+    let randomCharacter =
+      randomArray[Math.floor(Math.random() * randomArray.length)];
+
+    passwordArray.push(randomCharacter);
+  }
+  console.log(passwordArray);
+
   // Create a variable to store our generated password
-  var password = "";
+  const password = passwordArray.join("");
 
   // Return our created password
   return password;
