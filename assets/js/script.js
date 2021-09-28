d7d1310928e336748889fbfbd88aa4dbf96bff4a
liana valeria <liana.valeria15@gmail.com>
2021-09-28 09:29:25 +0100

    added prompt to get password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,113 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// store arrays of character options here
+let lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharacterArray = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "/",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
 function generatePassword() {
-  // PLEASE ADD ALL YOUR CODE HERE
-  // store arrays of character options here
   // prompt user to choose password length
+  const passwordLength = prompt("How many characters should the password be?");
+  console.log(passwordLength);
   /* validation for password length, if password length meets criteria 
   add confirm functions for user to pick the type of characters and store into a 
   variable
   */
+  if (passwordLength >= 8 && passwordLength <= 128) {
+  } else {
+  }
   // validation at least one type of character was chosen, each answer will be added to an object, if not alert and break
   //  declare a new variable, in which the password will be stored: pick a random array of characters, from which pick a random character and push to the new array
+  //convert the array to a string and display password for user
 }
 
 // Write password to the #password input
